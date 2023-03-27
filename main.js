220e226b4080a739eeaaa74849ebb21f6204ee8a
Rikesh Dahal <85576731+rikeshdahal@users.noreply.github.com>
2023-03-27 23:11:42 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+});
+
+
+const cards = document.querySelectorAll('.img');
+
+
+function dark(){
+    alert("test");
+}
+const darkModeControl = () => {
+    const darkCheck = document.getElementById('night-light-checkbox');
+  
+    darkCheck.addEventListener('click', () => {
+      if (darkCheck.checked) {
+        alert("test")
+      } else {
+        alert("test")
+      }
+    })
+  
+    
+  }
+  
+  
